b61efa5e6079e7e0970d40ac03b470027cced43f
Rudnei Santiago <rudnei.santiago@gmail.com>
2023-11-14 12:09:25 -0300

    Creating auth basic routes and ingress controller

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { json } from "body-parser";
 
+import {
+  currentUserRouter,
+  signinRouter,
+  signoutRouter,
+  signupRouter,
+} from "./routes";
+
 const app = express();
 app.use(json());
 
+app.use([currentUserRouter, signinRouter, signoutRouter, signupRouter]);
+
 app.listen(3000, () => {
-  console.log("Listen on port 3000!");
+  console.log("Listen on port 3000");
 });
