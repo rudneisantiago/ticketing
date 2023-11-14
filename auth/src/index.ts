d17769bc8ecd860b85431ef51b3a6b6b848e42d5
Rudnei Santiago <rudnei.santiago@gmail.com>
2023-11-14 16:46:33 -0300

    Handling async error with express-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 
 import { errorHandler } from "./middlewares/error-handler";
@@ -19,7 +20,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", () => {
+app.all("*", async (req, res, next) => {
   throw new NotFoundError();
 });
 
