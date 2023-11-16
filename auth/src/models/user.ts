393069cf8e6eaea8ac52ac9010aa3df6262c26a4
Rudnei Santiago <rudnei.santiago@gmail.com>
2023-11-16 17:31:39 -0300

    Implement user signup

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,24 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+// Interface used to CREATE a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Interface that describes the properties that user MODEL has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface that describes the properties that user DOCUMENTT has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  //   createdAt: string;
+  //   updatedAt: string;
+}
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -11,6 +31,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashedPassword = await Password.toHash(this.get("password"));
+    this.set("password", hashedPassword);
+  }
+
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
+// node imports
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-
+// app imports
+import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -15,7 +17,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Senha precisa ter entre 4 e 20 caracteres"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -24,10 +26,19 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError("Email already in use");
+    }
+
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
