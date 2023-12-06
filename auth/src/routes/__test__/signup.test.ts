e5aae0cecc5a4260afec7316f4f2f95a55f1dae2
Rudnei Santiago <rudnei.santiago@gmail.com>
2023-12-06 15:25:22 -0300

    setup MongoMemoryServer and signup test

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,44 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "teste@teste.com",
+      password: "1234",
+    })
+    .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "teste@",
+      password: "1234",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "teste@teste.com",
+      password: "123",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "teste@teste.com" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ password: "1234" })
+    .expect(400);
+});
