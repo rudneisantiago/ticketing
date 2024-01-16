063121f3e3840be102ca90c78744b91184374bae
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-01-16 18:04:57 -0300

    setup onSuccess signup function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,20 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ color }) => {
+  console.log(color);
   return <h1>Olá mundo!</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios
+    .get(
+      "https://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser"
+    )
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  return { color: "red" };
+};
+
+export default LandingPage;
