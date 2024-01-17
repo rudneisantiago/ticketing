d5975c064480a8364d9a50a71d1a2d2b7a323313
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-01-17 17:53:22 -0300

    getting initial props in app context

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,27 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div className="bg-dark-subtle vh-100">
+      <header className="py-4 px-4 bg-white ">My Header</header>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
+  return data;
+};
+
+export default AppComponent;
