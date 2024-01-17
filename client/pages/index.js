fb51e547500c95f4f985964e294fd8383c422a41
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-01-17 14:35:36 -0300

    manage browser and server requisitions

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,31 @@
 import axios from "axios";
 
-const LandingPage = ({ color }) => {
-  console.log(color);
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Olá mundo!</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios
-    .get(
-      "https://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser"
-    )
-    .catch((err) => {
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    const { data } = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser").catch((err) => {
       console.log(err.message);
     });
 
-  return { color: "red" };
+    return data;
+  }
 };
 
 export default LandingPage;
