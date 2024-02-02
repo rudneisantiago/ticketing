8e914a0535a5217bb275e8657e712b18de4747f7
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-02-02 17:29:41 -0300

    Create tickets service basic structure

---
+++ tickets/src/index.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  try {
+    await mongoose.connect("mongodb://tickets-mongo-srv:27017/auth");
+    console.log("MongoDb connection succeeded");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listen on port 3000");
+  });
+};
+
+start();
