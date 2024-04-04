9f1e6614a85a3bba6c503fe21ec8a2e63edb07c2
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-04-04 17:05:52 -0300

    Fixing test config

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -4,7 +4,7 @@ import request from "supertest";
 import { app } from "../app";
 
 declare global {
-  var signup: () => Promise<string[]>;
+  var signup: () => Promise<string[] | undefined>;
 }
 
 let mongo: MongoMemoryServer;
