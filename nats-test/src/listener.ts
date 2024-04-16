a5121696e127a5b019f97e88cb69fb6eb2dc53b0
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-04-16 16:12:11 -0300

    Basic functionality of nats

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,9 +1,21 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
 
-const stan = nats.connect("ticketing", "abc", {
-  url: "http://localhost:8222",
+console.clear();
+
+const stan = nats.connect("ticketing", "123", {
+  url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("listener connected to NATS");
+
+  const subscription = stan.subscribe("ticket:created");
+
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data:${data}`);
+    }
+  });
 });
