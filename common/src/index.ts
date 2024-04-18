ce401c5e8ce4534bb8bec6ec2738ecac69635a04
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-04-18 10:34:50 -0300

    add nats base classes to common

--- common/src/index.ts
+++ common/src/index.ts
@@ -11,3 +11,10 @@ export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
+
+/** Events import */
+export * from "./events/base-listener";
+export * from "./events/base-publisher";
+export * from "./events/subjects";
+export * from "./events/ticket-created-events";
+export * from "./events/ticket-updated-event";
