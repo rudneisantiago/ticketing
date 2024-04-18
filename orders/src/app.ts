c5dbf827e7bdf384fd00aec8e822335a1f47aead
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-04-18 15:26:54 -0300

    Setup orders service and update skaffold

---
+++ orders/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@rgsticketing/common";
+
+// import {
+//   createTicketRouter,
+//   indexTicketRouter,
+//   showTicketRouter,
+//   updateTicketRouter,
+// } from "./routes";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+// app.use([
+//   createTicketRouter,
+//   showTicketRouter,
+//   indexTicketRouter,
+//   updateTicketRouter,
+// ]);
+
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
