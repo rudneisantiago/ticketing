96ca4694c2d56870bc72a780431424c6670b4996
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-09-19 18:00:36 -0300

    set cancelled status when deleted

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,6 +1,5 @@
 import request from "supertest";
 import { Ticket } from "../../models/ticket";
-import { Order } from "../../models/orders";
 import { app } from "../../app";
 
 const createTicket = async () => {
@@ -18,12 +17,40 @@ it("fetch orders for an particular user", async () => {
   const ticket2 = await createTicket();
   const ticket3 = await createTicket();
 
-  const userOne = global.signin();
-  const userTwo = global.signin();
+  const user1 = global.signin();
+  const user2 = global.signin();
 
+  // UserOne
   await request(app)
     .post("/api/orders")
-    .set("Cookie", userOne)
+    .set("Cookie", user1)
     .send({ ticketId: ticket1.id })
     .expect(201);
+
+  // UserOne
+  const { body: orderOne } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user2)
+    .send({ ticketId: ticket2.id })
+    .expect(201);
+
+  const { body: orderTwo } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user2)
+    .send({ ticketId: ticket3.id })
+    .expect(201);
+
+  const response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", user2)
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[0].ticket.id).toEqual(ticket2.id);
+  expect(response.body[0].id).toEqual(orderOne.id);
+
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticket3.id);
 });
---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,53 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("fetches the order", async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: "Ticket",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it("returns an error if one user tries to fetch another users order", async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: "Ticket",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // Creates an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Fetch created order
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
